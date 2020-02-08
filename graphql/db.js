637d7ebeea08544443e46c7be043df9e539450c0
doxxitxxyoung <desertbeagle11@gmail.com>
2020-02-08 21:36:21 +0900

    finished all

--- graphql/db.js
+++ graphql/db.js
@@ -1,51 +1,48 @@
-//export const movies = [
-export let movies = [
-    {
-        id: 0,
-        name: "A",
-        score: 18
-    },
-    {
-        id: 1,
-        name: "B",
-        score: 19
-    },
+import axios from "axios";
+const BASE_URL = "https://yts.mx/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-    {
-        id: 2,
-        name: "C",
-        score: 20
-    },
-    {
-        id: 3,
-        name: "D",
-        score: 21
-    },
-
-];
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
+export const getMovies = async (limit, rating) => {
+    const {
+        data: {
+            data: { movies }
+        }   
+    } = await axios(LIST_MOVIES_URL, {
+        params: {
+            limit,
+            minimum_rating: rating
+        }
+    });
+    return movies;
 };
 
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
-    }
+export const getMovie = async (id) => {
+    const {
+        data: {
+            data: { movie }
+        }
+    } = await axios(MOVIE_DETAILS_URL, {
+        params: {
+            movie_id: id
+        }
+    });
+
+    return movie;
+
 };
 
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: parseInt(`${movies.length + 1}`),
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-}
+export const getSuggestions = async (id) => {
+    const {
+        data: {
+            data: { movies }
+        }
+    } = await axios(MOVIE_SUGGESTIONS_URL, {
+        params: {
+            movie_id : id
+        }
+    });
 
+    return movies;
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,26 +1,12 @@
-/*
-import { people, getById } from "./db";
+
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 const resolvers = {
     Query: {
-        people: () => people,
-        person: (_, { id }) => getById(id)
+        movies: (_, {limit, rating}) => getMovies(limit, rating),
+        movie: (_, { id }) => getMovie(id),
+        suggestions:(_, { id } ) => getSuggestions(id) 
     }
 };
-*/
-
-import { movies, getById, addMovie, deleteMovie} from "./db";
-
-const resolvers = {
-    Query: {
-        movies: () => movies,
-        movie: (_, { id }) => getById(id)
-    },
-    Mutation: {
-        addMovie: (_, {name, score}) => addMovie(name, score),
-        deleteMovie: (_, {id}) => deleteMovie(id)
-    }
-
-}
 
 export default resolvers;
